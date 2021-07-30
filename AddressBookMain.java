88a04c97bef60dd50ecbf137951b7beb851ef5c1
KratiGupta <kratigupta8545@gmail.com>
2021-07-30 22:36:58 +0530

    Added contact in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,74 @@
 package addressBookSystem;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class AddressBookMain {
- public static void main(String[] args) {
-	 System.out.println("Welcome to Address Book Master Branch");
- }
+	      
+	  
+	        private String firstName;
+	        private String lastName;
+	        private String address;
+	        private String city;
+	        private String state;
+	        private int zip;
+	        private long phoneNumber;
+	        private String email;
+	      
+
+	        void getEntry()          
+	        {
+	      
+	            Scanner sc = new Scanner(System.in);
+
+	            System.out.print("\n\tEnter First Name : ");
+	            firstName = sc.nextLine();
+
+	            System.out.print("\n\tEnter Last Name : ");
+	            lastName = sc.nextLine();
+
+	            System.out.print("\n\tEnter Address : ");
+	            address = sc.nextLine();
+
+	            System.out.print("\n\tEnter City : ");
+	            city  = sc.nextLine();
+
+	            System.out.print("\n\tEnter State : ");
+	            state  = sc.nextLine();
+	            
+	            System.out.println("\n\tEnter Email : ");
+	            email =sc.nextLine();
+
+	            System.out.print("\n\tEnter Zip : ");
+	            zip = sc.nextInt();
+
+	            System.out.print("\n\tEnter Phone Number : ");
+	            phoneNumber = sc.nextLong();
+	            
+	            }
+	            
+	            public void readEntry(){
+	            System.out.println("First Name:"+firstName );
+	            System.out.println("Last Name:"+lastName );
+	            System.out.println("Address:"+address );
+	            System.out.println("City:"+city );
+	            System.out.println("State:"+state);
+	            System.out.println("Email:"+email );
+	            System.out.println("Zip:"+zip );
+	            System.out.println("Phone Number:"+phoneNumber );
+	          
+	        }
+	   
+
+            public static void main(String[] args) {
+         	   
+         	   System.out.println("Welcome to Address Book Master Branch");
+         	   
+         	   AddressBookMain a = new AddressBookMain();
+         	   
+         	   a.getEntry();
+         	   a.readEntry();
+         	   
+            }
 }
+
